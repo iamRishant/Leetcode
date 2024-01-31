d9586b6c816023a3726e78221dc49646fa6cbd3c
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-01-31 19:12:09 +0530

    Time: 218 ms (34.00%) | Memory: 119.4 MB (16.62%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long,long long>> > pq;
+        long long sum=0;
+        long long ans=1e9;
         int n=nums.size();
-        long long preFixSum=0;
-        long long ans=INT_MAX;
-        deque<pair<long long,long long>> dq;//{prefixsum,index}
-        dq.push_back({0,-1});
 
+        pq.push({0,-1});
         for(int i=0;i<n;i++){
-            preFixSum+=nums[i];
-            while(!dq.empty() && preFixSum<dq.back().first) dq.pop_back();
-            long long rem=preFixSum-k;
-            while(!dq.empty() && rem>=dq.front().first){
-                ans=min(ans,i-dq.front().second);
-                dq.pop_front();
+            sum+=nums[i];
+
+            long long rem=sum-k;
+            while(!pq.empty() && rem>=pq.top().first){
+                ans=min(ans,i-pq.top().second);
+                pq.pop();
             }
-            dq.push_back({preFixSum,i});
+            pq.push({sum,i});
         }
-
-        return ans==INT_MAX? -1:ans;
-
+        return ans==1e9? -1 : ans;
     }
 };
