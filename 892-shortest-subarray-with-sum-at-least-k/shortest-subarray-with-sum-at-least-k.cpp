74138398c96cc76ba7237a4d816533f41fddba32
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-01-31 19:01:04 +0530

    Time: 181 ms (66.00%) | Memory: 112.4 MB (31.69%) - LeetSync

---
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n=nums.size();
+        long long preFixSum=0;
+        long long ans=INT_MAX;
+        deque<pair<long long,long long>> dq;//{prefixsum,index}
+        dq.push_back({0,-1});
+
+        for(int i=0;i<n;i++){
+            preFixSum+=nums[i];
+            while(!dq.empty() && preFixSum<dq.back().first) dq.pop_back();
+            long long rem=preFixSum-k;
+            while(!dq.empty() && rem>=dq.front().first){
+                ans=min(ans,i-dq.front().second);
+                dq.pop_front();
+            }
+            dq.push_back({preFixSum,i});
+        }
+
+        return ans==INT_MAX? -1:ans;
+
+    }
+};
