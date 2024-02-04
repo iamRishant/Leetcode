4cf65f8d97fd56440735c84f7d01ff68995d60a0
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-02-04 12:01:48 +0530

    Time: 366 ms (60.00%) | Memory: 135.4 MB (100.00%) - LeetSync

---
+++ 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        map<long long,long long> mp;
+        long long currSum=0;
+        long long ans=LLONG_MIN;
+        for(int i=0;i<nums.size();i++){
+            long long rem1=nums[i]-k;
+            long long rem2=nums[i]+k;
+            if(mp.find(rem1)!=mp.end()){
+                long long val=currSum+nums[i]-mp[rem1];
+                ans=max(ans,val);
+            }
+            if(mp.find(rem2)!=mp.end()){
+                long long val=currSum+nums[i]-mp[rem2];
+                ans=max(ans,val);
+            }
+
+            if(mp.find(nums[i])==mp.end()) mp[nums[i]]=currSum;
+            else{
+                mp[nums[i]]=min(mp[nums[i]],currSum);
+            }
+
+            currSum+=nums[i];
+        }
+        return ans==LLONG_MIN ? 0:ans;
+    }
+};
