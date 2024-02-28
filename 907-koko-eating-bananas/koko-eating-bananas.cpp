5b316fe8c4e4306a59ddca2667ec2fc72927ca1f
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-02-28 19:06:23 +0530

    Time: 42 ms (24.68%) | Memory: 37.5 MB (5.13%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -12,19 +12,25 @@ public:
         
     }
     int minEatingSpeed(vector<int>& piles, int hi) {
-        sort(piles.begin(),piles.end());
         int n=piles.size();
+        // lets find the max range
+        int maxi=0;
+        for(int i=0;i<n;i++) maxi=max(piles[i],maxi);
+        // now we need to find a value between 1 and maxi
         int l=1;
-        int h=piles[n-1];
+        int h=maxi;
+        // let current ans be max
         int ans=h;
         while(h>=l){
             int mid=l+(h-l)/2;
 
             int val=check(mid,piles,hi);
             if(val==2) {
+                // if the current mid cannot satisfy the timelimit then we will try to eat more at a time
                 l=mid+1;
                 }
             else {
+                // if current k value is sufficient then we will go more back to check for any lesser value
                 ans=min(mid,ans);
                 h=mid-1;
                 };
