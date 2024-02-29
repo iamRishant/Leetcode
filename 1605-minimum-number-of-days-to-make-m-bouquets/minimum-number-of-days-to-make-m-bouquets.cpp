dcedd86c3ca2833f9ae861df161308b77788457a
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-02-29 16:26:58 +0530

    Time: 103 ms (93.29%) | Memory: 69 MB (22.31%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int check(int mid,vector<int> &arr,int m,int k){
+        int ans=0;
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=mid) count++;
+            else{
+                // checking for consecutive blooms and ans==m then we got the answer
+                ans+=((count)/k);
+                count=0;
+            }
+        }
+        ans+=(count/k);
+        // cout<<"ans "<<ans<<" ";
+        if(ans>=m) return 0;
+        return 1;
+    }
+    int minDays(vector<int>& arr, int m, int k) {
+        int n=arr.size();
+        if(long(m)*k>n) return -1;
+        int l=INT_MAX;
+        int h=-1;
+        for(int i=0;i<n;i++){
+            l=min(arr[i],l);
+            h=max(arr[i],h);
+        }
+        int ans=h;
+        // now we got thee range we have to find the correct op
+        while(h>=l){
+            // cout<<"HELLO"<<endl;
+            int mid=l+(h-l)/2;
+            int val=check(mid,arr,m,k);
+            // cout<<"mid "<<mid<<" ";
+            cout<<"val "<<val<<" ";
+            if(val==0){
+                ans=min(mid,ans);
+                h=mid-1;
+            }
+            else l=mid+1;
+        }
+
+        return ans;
+    }
+};
