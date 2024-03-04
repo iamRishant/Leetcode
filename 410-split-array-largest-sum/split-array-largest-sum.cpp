f697dd96d32aa2efa398ae5b33331fe0accaec7e
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-03-04 11:23:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (5.03%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool check(long long i, vector<int> nums,int k){
+        int student=1;
+        int numberOfPages=0;
+        for(int j=0;j<nums.size();j++){
+            if(numberOfPages+nums[j]<=i){
+                numberOfPages+=nums[j];
+            }
+            else{
+                // goto  next student
+                student++;
+                numberOfPages=nums[j];
+            }
+        }
+        if(student<=k) return 0;
+        return 1;
+        
+    }
+    int splitArray(vector<int>& nums, int k) {
+        // this is same as book allocation problem
+        int low=-1;
+        for(int i=0;i<nums.size();i++) {low=max(nums[i],low);};
+        long long high=accumulate(nums.begin(),nums.end(),0);
+
+        int ans=high;
+        while(high>=low){
+            int mid=low+(high-low)/2;
+
+            bool val=check(mid,nums,k);
+            if(val){
+                low=mid+1;
+            }
+            else{
+                ans=min(mid,ans);
+                high=mid-1;
+            }
+        }
+        return ans;
+
+
+    }
+};
