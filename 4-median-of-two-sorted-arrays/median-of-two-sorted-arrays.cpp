aa745f616a63d3f3574959f0665ee807aea6a29c
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-03-05 23:14:35 +0530

    Time: 35 ms (18.12%) | Memory: 94.5 MB (47.66%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,63 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // space optimised just playing with pointers
-        int i=0;
-        int j=0;
         int n1=nums1.size();
         int n2=nums2.size();
-        int ind1=(n1+n2)/2;
-        int ind2=ind1-1;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);//bas bakchodi krr rhe ki first array ka size kam rhe
+        int low=0;
+        int high=n1;
+        //minimum no element from array 1 and max is all element from array one
+        int leftElements=(n1+n2+1)/2;// agar odd bhi rhega to 3 element on left so that l1 and l2 ka max se mera nikal jaega
+        int ele1=-1;
+        int ele2=-1;
+        while(high>=low){
+            int mid1=low+(high-low)/2;
 
-        int ind1ele=-1;
-        int ind2ele=-1;
-        int count=0;
-        while(i<n1 && j<n2){
-            if(nums1[i]>nums2[j]){
-                if(count==ind1){
-                    ind1ele=nums2[j];
-                }
-                if(count==ind2){
-                    ind2ele=nums2[j];
-                }
-                j++;
-                count++;
-            }
-            else{
-                if(count==ind1){
-                    ind1ele=nums1[i];
-                }
-                if(count==ind2){
-                    ind2ele=nums1[i];
-                }
-                i++;
-                count++;
-            }
-        }
+            int mid2=leftElements-mid1;
 
-        while(i<n1){
-            if(count==ind1){
-                    ind1ele=nums1[i];
-                }
-                if(count==ind2){
-                    ind2ele=nums1[i];
-                }
-                i++;
-                count++;
+            int l1=INT_MIN;
+            int l2=INT_MIN;
+            int r1=INT_MAX;
+            int r2=INT_MAX;
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+
+            if(l1<=r2 && l2<=r1){
+                ele1=max(l1,l2);
+                ele2=min(r1,r2);
+                break;
+            }
+            else if(l1>r2){
+                high=mid1-1;
+            }
+            else low=mid1+1;
+
         }
-        while(j<n2){
-            if(count==ind1){
-                    ind1ele=nums2[j];
-                }
-                if(count==ind2){
-                    ind2ele=nums2[j];
-                }
-                j++;
-                count++;
-        }
-        if((n1+n2)%2!=0){
-            return ind1ele;
-        }
-        return (double)(ind1ele+ind2ele)/2.0;
+        if((n1+n2)%2!=0) return ele1;
+        
+        return (double)(ele1+ele2)/2.0;
+
+
     }
 };
