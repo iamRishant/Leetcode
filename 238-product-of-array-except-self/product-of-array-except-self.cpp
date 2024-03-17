93f02eaa804ff790b3dcab5cb15c1de7a92549c9
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-03-17 14:43:32 +0530

    Time: 17 ms (45.73%) | Memory: 27.4 MB (24.32%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -9,20 +9,13 @@ public:
             pre[i]=currProd*nums[i];
             currProd=pre[i];
         }
-        currProd=1;
-        int ind=0;
-        for(int i=n-1;i>=0;i--){
-            suff[i]=currProd*nums[i];
-            currProd=suff[i];
-        }
-        
         vector<int> ans(n);
-        for(int i=0;i<n;i++){
+        currProd=1;
+        for(int i=n-1;i>=0;i--){
             int left=1;
-            int right=1;
             if(i>0) left=pre[i-1];
-            if(i<n-1) right=suff[i+1];
-            ans[i]=left*right;
+            ans[i]=left*currProd;
+            currProd*=nums[i];
         }
         return ans;
     }
