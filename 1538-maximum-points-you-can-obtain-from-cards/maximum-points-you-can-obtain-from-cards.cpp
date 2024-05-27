9a42940d05d209b0132a1607e5606f431641c814
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-05-27 14:44:46 +0530

    Time: 50 ms (12.74%) | Memory: 48.1 MB (5.16%) - LeetSync

---
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cp, int k) {
+        int n=cp.size();
+        vector<int> preFix(cp.size(),0);
+        vector<int> postFix(cp.size(),0);
+        preFix[0]=cp[0];
+        for(int i=1;i<cp.size();i++){
+            preFix[i]=preFix[i-1]+cp[i];
+            // cout<<preFix[i]<<" ";
+        }
+        // cout<<endl;
+        postFix[0]=cp[n-1];
+        int j=1;
+        for(int i=n-2;i>=0;i--){
+            postFix[j]=postFix[j-1]+cp[i];
+            // cout<<postFix[j]<<" ";
+            j++;
+            
+        }
+        int ans=0;
+        int l=k-1;
+        int r=-1;
+        k++;
+        while(k--){
+            if(r==-1){
+                ans=preFix[l];
+            }
+            else if(l==-1){
+                ans=max(ans,postFix[r]);
+            }
+            else{
+                ans=max(preFix[l]+postFix[r],ans);
+            }
+            r++;
+            l--;
+        }
+        return ans;
+
+    }
+};
