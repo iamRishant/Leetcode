d03dabe05180890ca4636658c06f1e8aee1f94d1
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-05-29 12:46:40 +0530

    Time: 217 ms (5.23%) | Memory: 68.6 MB (5.11%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(vector<int> nums,int k){
+        if(k<0) return 0;
+        map<int,int> mp;
+        int l=0;
+        int r=0;
+        int ans=0;
+        while(r<nums.size()){
+            mp[nums[r]]++;
+            while(mp.size()>k){
+                mp[nums[l]]--;
+                if(mp[nums[l]]==0) mp.erase(nums[l]);
+                l++;
+            }
+
+            ans+=(r-l+1);
+            r++;
+        }
+        return ans;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        // same concept of <=k and <=k-1
+        return solve(nums,k)-solve(nums,k-1);
+    }
+};
