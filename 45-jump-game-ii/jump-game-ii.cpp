8c61f7e8eddda29b484497085430c4f611f6c88b
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-07-01 18:15:17 +0530

    Time: 7 ms (88.54%) | Memory: 18.9 MB (61.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int solve(int index,vector<int> &nums,int n,vector<int> &dp){
-        if(index+nums[index]>=n-1) return 1;
-        if(nums[index]==0) return 1e9;
-
-        if(dp[index]!=-1) return dp[index];
-
-        int ans=INT_MAX;
-        for(int i=index+1;i<=min(index + nums[index], n - 1);i++){
-            int temp=1+solve(i,nums,n,dp);
-            ans=min(ans,temp);
-        }
-        return dp[index]= ans;
-    }
     int jump(vector<int>& nums) {
+        if(nums.size()==1) return 0;
+        if(nums[0]>=nums.size()-1) return 1;
+        int jump=0;
+        int index=0;
+        int currRange=nums[0];
+        int maxRange=nums[0];
         int n=nums.size();
-        if(nums[0]==0 || n==1) return 0;
-        // if(n==1) return 1;
-        vector<int> dp(n+1,-1);
-        return solve(0,nums,n,dp);
+        while(index<nums.size()){
+            maxRange=max(maxRange,index+nums[index]);
+
+            if(index==currRange){
+                currRange=maxRange;
+                jump++;
+                if(maxRange>=n-1) return jump+1;
+                
+            }
+            index++;
+        }
+        return jump;
     }
 };
