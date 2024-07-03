444779a687c30fd8a16fc47a3b5f5912b17eac48
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-07-03 17:42:58 +0530

    Time: 11 ms (64.71%) | Memory: 20.4 MB (74.11%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n = intervals.size(), i = 0;
-        vector<vector<int>> res;
-        //case 1: no overlapping case before the merge intervals
-		//compare ending point of intervals to starting point of newInterval
-        while(i < n && intervals[i][1] < newInterval[0]){
-            res.push_back(intervals[i]);
-            i++;
-        }                           
-		//case 2: overlapping case and merging of intervals
-        while(i < n && newInterval[1] >= intervals[i][0]){
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+
+        // first jo jo overlap nhi kar rha usko as it it daal do
+        int i=0;
+        int n=intervals.size();
+        while(i<n && newInterval[0]>intervals[i][1]){
+            ans.push_back({intervals[i][0],intervals[i][1]});
             i++;
         }
-        res.push_back(newInterval);
-        // case 3: no overlapping of intervals after newinterval being merged
-        while(i < n){
-            res.push_back(intervals[i]);
+        // now jo overlapping hai usko merge krdo
+        while(i<n && newInterval[1]>=intervals[i][0]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
             i++;
         }
-        return res;
+        // when we merged the over lapping thiing we will just add it 
+        ans.push_back(newInterval);
+        // after that just simply add remaining
+        while(i<n){
+            ans.push_back({intervals[i][0],intervals[i][1]});
+            i++;
+        }
+        
+
+        return ans;
+
     }
 };
