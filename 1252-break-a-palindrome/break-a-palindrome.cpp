0fd99a306d4b984fc1d618c5907afdbf25dddcbd
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-07-12 16:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (10.81%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.cpp
+++ 1252-break-a-palindrome/break-a-palindrome.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
     string breakPalindrome(string palindrome) {
-        // i need to find smallest character other than a then just convert it to a
-        int index=-1;
-        int n=palindrome.size()/2;
-        char minchar='z'+1;
         if(palindrome.size()==1) return "";
-        for(int i=0;i<n;i++){
+        for(int i=0;i<palindrome.size()/2;i++){
             if(palindrome[i]!='a'){
-                // cout<<minchar<<endl;
-                if(minchar>palindrome[i]){
-                    minchar=palindrome[i];
-                    index=i;
-                    break;
-                }
+                palindrome[i]='a';
+                return palindrome;
             }
         }
-
-        string ans="";
-        if(index==-1){
-            // it means only a is there
-            for(int i=0;i<palindrome.size()-1;i++) {
-                ans+=palindrome[i];
-            }
-            ans+='b';
-        }
-        else{
-            for(int i=0;i<palindrome.size();i++){
-                if(i==index){
-                    ans+='a';
-                }
-                else ans+=palindrome[i];
-            }
-        }
-            return ans;
+        int n=palindrome.size()-1;
+        palindrome[n]='b';
+        return palindrome;
     }
 };
