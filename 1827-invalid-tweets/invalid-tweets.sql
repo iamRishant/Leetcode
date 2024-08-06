68ef71bd88009a44cde962c1491aff3f62df3720
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-08-06 20:48:44 +0530

    Time: 604 ms (36.17%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select tweet_id from Tweets where LENGTH(content)>15;
+select tweet_id from Tweets where CHAR_LENGTH(content)>15;
+
+-- length return size in bytes length('E') is 3 bytes while char_length return number of character char_length('e') is 1 
