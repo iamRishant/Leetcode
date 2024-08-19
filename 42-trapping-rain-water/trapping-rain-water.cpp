8b76fca59554019f7c1685e4c3793f0f17bb586e
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-08-19 23:15:07 +0530

    Time: 14 ms (30.57%) | Memory: 26 MB (6.99%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,26 @@
-class Solution { // 0 ms, faster than 100.00%
+class Solution {
 public:
     int trap(vector<int>& height) {
-        if (height.size() <= 2) return 0;
-        int n = height.size(), maxLeft = height[0], maxRight = height[n-1];
-        int left = 1, right = n - 2, ans = 0;
-        while (left <= right) {
-            if (maxLeft < maxRight) {
-                if (height[left] > maxLeft)
-                    maxLeft = height[left];
-                else
-                    ans += maxLeft - height[left];
-                left += 1;
-            } else {
-                if (height[right] > maxRight)
-                    maxRight = height[right];
-                else
-                    ans += maxRight - height[right];
-                right -= 1;
+        // total+=(min(leftMax,rightMax)-arr[i])
+        int n=height.size();
+        vector<int> preFixMax(n,0);
+        vector<int> sufFixMax(n,0);
+        preFixMax[0]=height[0];
+        sufFixMax[n-1]=height[n-1];
+        for(int i=1;i<n;i++){
+            preFixMax[i]=max(preFixMax[i-1],height[i]);
+            sufFixMax[n-i-1]=max(sufFixMax[n-i],height[n-i-1]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int l=preFixMax[i];
+            int r=sufFixMax[i];
+            if(height[i]<l && height[i]<r){
+                ans+=(min(l,r)-height[i]);
             }
         }
+
         return ans;
+
     }
 };
