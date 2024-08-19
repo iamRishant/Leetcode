92ac35615ef3f51fa0fd60a2201d585fa3e266e4
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-08-19 23:16:43 +0530

    Time: 15 ms (23.36%) | Memory: 25.4 MB (9.16%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,19 +3,21 @@ public:
     int trap(vector<int>& height) {
         // total+=(min(leftMax,rightMax)-arr[i])
 
-        // time is o(n) but space is o(2n) 
+        // time is o(n) and space is o(n)
         int n=height.size();
-        vector<int> preFixMax(n,0);
+        // vector<int> preFixMax(n,0);
         vector<int> sufFixMax(n,0);
-        preFixMax[0]=height[0];
+        // preFixMax[0]=height[0];
         sufFixMax[n-1]=height[n-1];
         for(int i=1;i<n;i++){
-            preFixMax[i]=max(preFixMax[i-1],height[i]);
+            // preFixMax[i]=max(preFixMax[i-1],height[i]);
             sufFixMax[n-i-1]=max(sufFixMax[n-i],height[n-i-1]);
         }
         int ans=0;
+        int l=height[0];
         for(int i=0;i<n;i++){
-            int l=preFixMax[i];
+            // int l=preFixMax[i];
+            l=max(height[i],l);
             int r=sufFixMax[i];
             if(height[i]<l && height[i]<r){
                 ans+=(min(l,r)-height[i]);
