d05c1404d7207f1e62d5211876886f9563a05330
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-09-15 11:33:08 +0530

    Time: 6 ms (56.91%) | Memory: 12.1 MB (57.85%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones=0;
-        int twos=0;
-        for(int i=0;i<nums.size();i++){
-            ones=((ones^nums[i]) & (~twos));
-            twos=((twos^nums[i]) & (~ones));
+        int result=0;
+
+        for(int k=0;k<32;k++){
+            int temp=(1<<k);
+            int countOfZ=0;
+            int countOfO=0;
+            for(int i=0;i<nums.size();i++){
+                // we will check kth bit of each number using & operator
+                if((nums[i]&temp)!=0) countOfO++;
+                else countOfZ++;
+            }
+            if((countOfO%3)==1){
+                result=(result|temp);
+            }
         }
-        return ones;
+        return result;
     }
 };
