c9bb04a8aa02dd124aebec0ce88dd9472fdb5bae
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-09-16 09:26:28 +0530

    Time: 3 ms (98.26%) | Memory: 18.7 MB (54.34%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,34 +3,34 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int person1=INT_MIN;
         int person2=INT_MIN;
-        int cPerson1=0;
-        int cPerson2=0;
-        for(int i=0;i<nums.size();i++){
-            if(cPerson1==0 && nums[i]!=person2){
-                cPerson1=1;
+        int cp1=0;
+        int cp2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(cp1==0 && nums[i]!=person2){
+                cp1=1;
                 person1=nums[i];
             }
-            else if(cPerson2==0 && nums[i]!=person1){
-                cPerson2=1;
+            else if(cp2==0 && nums[i]!=person1){
+                cp2=1;
                 person2=nums[i];
             }
-            else if(nums[i]==person1) cPerson1++;
-            else if(nums[i]==person2) cPerson2++;
-            else{
-                cPerson1--;
-                cPerson2--;
-            }
+            else if(nums[i]==person1) cp1++;
+            else if(nums[i]==person2) cp2++;
+            else {cp1--; cp2--;}
         }
-
-        cPerson1=0;
-        cPerson2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==person1) cPerson1++;
-            else if(nums[i]==person2) cPerson2++;
+        // now here we have two top majority element now we just have to check there count is greater than n/3
+        cp1=0;
+        cp2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==person1) cp1++;
+            else if(nums[i]==person2) cp2++;
         }
         vector<int> ans;
-        if(cPerson1>nums.size()/3) ans.push_back(person1);
-        if(cPerson2>nums.size()/3) ans.push_back(person2);
+        if(cp1>n/3) ans.push_back(person1);
+        if(cp2>n/3) ans.push_back(person2);
+
         return ans;
+        
     }
 };
