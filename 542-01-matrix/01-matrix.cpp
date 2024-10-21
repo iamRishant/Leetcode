8606dae50d83b91d5786269dfbe9b2781b8a7592
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-10-21 19:17:54 +0530

    Time: 19 ms (99.18%) | Memory: 38.2 MB (43.90%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        vector<vector<int>> mat2(mat.size(),vector<int>(mat[0].size(),0));
-        queue <pair<pair<int,int>,int>> q;
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[i].size();j++){
-                if(mat[i][j]==0) q.push({{i,j},0});
-            }
-        }
-        int row[]={-1,0,1,0};
-        int col[]={0,1,0,-1};
-        while(!q.empty()){
-            int f=q.front().first.first;
-            int s=q.front().first.second;
-            int d=q.front().second;
-            q.pop();
 
-            for(int i=0;i<4;i++){
-                int r=row[i]+f;
-                int c=col[i]+s;
-                if(r>=0&&r<mat.size()&&c>=0&&c<mat[r].size()&&mat[r][c]==1&&mat2[r][c]==0){
-                    mat2[r][c]=d+1;
-                    q.push({{r,c},d+1});
+        int n=mat.size();
+        int m=mat[0].size();
+
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> dis(n,vector<int>(m,0));
+
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
                 }
             }
         }
-        return mat2;
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int row=it.first.first;
+            int col=it.first.second;
+
+            int distance=it.second;
+
+            dis[row][col]=distance;
+            // vis[row][col]=1;
+
+
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol]){
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},distance+1});
+                }
+            }
+
+        }
+        return dis;
     }
 };
