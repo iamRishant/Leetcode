b3a97eaf03c1190f1c42b9f44080689215ff6fba
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-10-27 11:31:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (14.02%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -3,6 +3,9 @@ public:
     bool isUgly(int n) {
         if(n<=0) return false;
 
+        // agar n ko sirf 2 ,3 and 5 se divide krke 1 tk la skte hai it means it dont have anyfactors 
+        // other than 2,3 and 5
+
         while(n%2==0) n=n/2;
         while(n%3==0) n=n/3;
         while(n%5==0) n=n/5;
