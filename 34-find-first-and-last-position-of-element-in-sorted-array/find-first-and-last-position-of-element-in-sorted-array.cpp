c3615db5787d556bfeafae3ea43109955b26aee2
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-10-29 22:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (39.89%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int second=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        if(first==nums.size()) return {-1,-1};
-        if(nums.size()==0) return {-1,-1};
-        if(nums[first]!=target) return {-1,-1};
-        if(nums[first]==target && nums[second-1]==target) return {first,second-1};
-
-        return {-1,-1};
-
-
+        int occ=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        if(occ==-1 || occ==nums.size() || nums[occ]!=target) return {-1,-1};
+        int left=occ;
+        int right=upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
+        return {left,right};
     }
 };
