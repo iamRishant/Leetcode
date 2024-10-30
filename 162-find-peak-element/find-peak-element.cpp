3cf95aca5016039adbd5303f53cf81af050b6ec3
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-10-30 14:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (85.78%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return 0;
-        // first just check first and last element
-        if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int left=0;
+            int right=0;
+            if(mid==0) left=INT_MIN;
+            else left=nums[mid-1];
+            if(mid==n-1) right=INT_MIN;
+            else right=nums[mid+1];
 
-        // since we checked the first and last element then we reduced the search space
-        int s=1;
-        int e=n-2;
-        while(e>=s){
-            int mid=s+(e-s)/2;
-            // now three 3 cases
-            //1 mid is the peak
-            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return mid;
-            // so either on the decreasing side then peak on left
-            else if(nums[mid]<nums[mid-1] ) e=mid-1;
-            // either on the increasing side so peak on right
-            else if(nums[mid]<nums[mid+1]) s=mid+1;
+            if(nums[mid]>left && nums[mid]>right) return mid;
+            else if(nums[mid]>left && nums[mid]<right) low=mid+1;
+            else if(nums[mid]>right && nums[mid]<left) high=mid-1;
+            else low=mid+1;//mtlb lowest point par hai kahi jao left right peak mil hi jaega
         }
-        return -1;
-
-
+        return 0;
     }
 };
