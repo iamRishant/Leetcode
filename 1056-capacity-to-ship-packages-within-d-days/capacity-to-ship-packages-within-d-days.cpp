a5f19dce7e5e10cd1384aa329b926575f0683c84
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-11-01 14:45:26 +0530

    Time: 15 ms (37.19%) | Memory: 33.9 MB (11.55%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    int check(int mid,vector<int> &weights,int days){
-        int n=weights.size();
-        int temp=mid;
-        int count=0;
-        // int i=1;
-        for(int i=0;i<n;i++){
-            temp=temp-weights[i];
-            // cout<<temp<<" ";
-            if(temp<0){
-                count++;
-                temp=mid-weights[i];
-                if(temp<0) return 1;
+    bool check(int mid,vector<int> & weights,int days){
+        int curr=0;
+        int day=1;
+        for(int i=0;i<weights.size();i++){
+            curr+=weights[i];
+            if(weights[i]>mid) return false;
+            if(curr>mid){
+                day++;
+                curr=weights[i];
             }
+            
         }
-        if(temp>=0) count++;
-        
-        if(count<=days) return 0;
-        return 1;
+
+        if(day<=days) return true;
+        return false;
     }
     int shipWithinDays(vector<int>& weights, int days) {
+        int low=1e9;
+        int high=0;
         int n=weights.size();
-        vector<int> preFix(n+1,0);
 
-        for(int i=1;i<=n;i++){
-            preFix[i]=weights[i-1]+preFix[i-1];
+        for(int i=0;i<n;i++){
+            low=min(low,weights[i]);
+            high+=weights[i];
         }
-        int l=preFix[0];
-        int h=preFix[n];
-        int ans=h;
-        while(h>=l){
-            int mid=l+(h-l)/2;
+        
 
-            int val=check(mid,weights,days);
-            if(val==0) {h=mid-1; ans=min(ans,mid);}
-            else l=mid+1;
-        }
+        int ans=high;
 
-        return ans;
+         while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(mid,weights,days)){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+         }
 
+         return ans;
     }
 };
