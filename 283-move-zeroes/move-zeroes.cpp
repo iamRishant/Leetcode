5322d810fc6219789ce3baf5a66667b0b26fef1d
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-11-06 15:38:20 +0530

    Time: 4 ms (15.71%) | Memory: 22 MB (26.49%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left=0;
         int n=nums.size();
-        while(left<n){
-            while(left<n && nums[left]!=0) left++;
-            // now here left is pointing to 0
-            int right=left+1;
-            while(right<n &&nums[right]==0) right++;
-            // now here right is just pointing a non zero number 
-            
-            if(left>=n || right>=n) break;
-            swap(nums[left],nums[right]);
-            left++;
+        int curr=0;
+        int start=0;
+
+        while(curr<n){
+            if(nums[curr]!=0){
+                swap(nums[curr],nums[start]);
+                start++;
+            }
+            curr++;
         }
     }
 };
