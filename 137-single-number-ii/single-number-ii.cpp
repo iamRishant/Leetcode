a5b03ad6f9ceae334a0d0aec8c827e2833b5d830
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-11-06 23:48:45 +0530

    Time: 3 ms (37.13%) | Memory: 12.1 MB (86.24%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result=0;
-
+        // we will build answer bit by bit
         for(int k=0;k<32;k++){
             int temp=(1<<k);
-            int countOfZ=0;
+            // int countOfZ=0;
             int countOfO=0;
             for(int i=0;i<nums.size();i++){
                 // we will check kth bit of each number using & operator
                 if((nums[i]&temp)!=0) countOfO++;
-                else countOfZ++;
+                // else countOfZ++; //no use counting number of 0
             }
+            // if number of 0s are odd no need to do anything but if number of 1s are odd
+            // then we have to update our answer bit
             if((countOfO%3)==1){
                 result=(result|temp);
             }
