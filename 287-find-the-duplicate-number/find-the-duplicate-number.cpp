b0e00ade234ce59161a4f83f5ff395b51455a214
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-11-07 11:52:38 +0530

    Time: 1 ms (76.94%) | Memory: 63.9 MB (82.04%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // tortoise and hare method
-        int slow=nums[0];
-        int fast=nums[0];
+        int slow=0;
+        int fast=0;
         while(true){
             slow=nums[slow];
             fast=nums[nums[fast]];
-            if(fast==slow){
-                slow=nums[0];
-                while(fast!=slow){
+
+            if(slow==fast){
+                fast=0;
+                while(slow!=fast){
                     slow=nums[slow];
                     fast=nums[fast];
-
                 }
-                return fast;
+                return slow;
             }
         }
-        return 0;
+        return -1;
     }
 };
