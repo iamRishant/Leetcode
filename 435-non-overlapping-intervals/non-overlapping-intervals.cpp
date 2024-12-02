6157ba09e669339cf3f385828bf379b59f849aad
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-02 17:36:04 +0530

    Time: 63 ms (36.90%) | Memory: 93.8 MB (32.00%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-    static bool comp(vector<int> &first,vector<int> &second){
-        if(first[0]==second[0]){
-            return second[1]>first[1];
-        }
-        return second[0]>first[0];
+    static bool comp(vector<int> & first, vector<int> &second){
+        if(first[0]==second[0]) return first[1]<second[1];
+
+        return first[0]<second[0];
     }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),comp);
-        int mini=intervals[0][1];
-        int count=0;
 
+        sort(intervals.begin(),intervals.end(),comp);
+
+        int count=0;
+        int mini=intervals[0][1];
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]>=mini){
-                mini=intervals[i][1];
-            }
-            else{
+            if(intervals[i][0]<mini){
+                // when we get overlapping then now we have to decide ki 
+                // konsa wlae ko remove krna hai
+                //to thoda sochne par pta chalega ki 
+                // jiska range jyda usko htao kyuki wo jyda logo se overlap krega
+                //to jiska range minimum hai usko rakhenge and and jiska jyda hai usko hta kar 
+                // count++ kr denge
                 count++;
-                mini=min(mini,intervals[i][1]);
+                mini=min(intervals[i][1],mini);
+                
             }
+            else mini=intervals[i][1];
         }
         return count;
+        
     }
 };
