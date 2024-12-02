09dded3ea8b38a1ef75fbee4c1d2b1900fe875c1
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-02 16:43:25 +0530

    Time: 4 ms (42.49%) | Memory: 15.1 MB (41.46%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    static bool comp(vector<int> &first,vector<int> &second){
+    static bool comp(vector<int> & first, vector<int> &second){
         if(first[0]==second[0]) return first[1]>second[1];
+
         return first[0]<second[0];
     }
-
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),comp);
+
         int count=0;
         int maxi=intervals[0][1];
         for(int i=1;i<intervals.size();i++){
-            if(intervals[i][1]<=maxi) count++;
-            maxi=max(intervals[i][1],maxi);
+            if(intervals[i][1]<=maxi){
+                count++;
+            }
+            else maxi=max(maxi,intervals[i][1]);
         }
         return intervals.size()-count;
     }
