5dbb14da33faf486f0fc3fcfb0cc6163dd8604a9
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-02 15:01:19 +0530

    Time: 14 ms (7.63%) | Memory: 25.8 MB (5.08%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        if(firstList.size()==0 || secondList.size()==0) return {};
+     vector<vector<int>> store;
+     for(int i=0;i<firstList.size();i++){
+        store.push_back(firstList[i]);
+     }   
+     for(int i=0;i<secondList.size();i++){
+        store.push_back(secondList[i]);
+     }
+     sort(store.begin(),store.end());
 
-        vector<vector<int>> ans;
+     int prev=0;
+     vector<vector<int>> ans;
+    //  int curr=1;
+    for(int i=1;i<store.size();i++){
+        if(store[i][0]>store[prev][1]){
 
-        int i=0;
-        int j=0;
-        int n=firstList.size();
-        int m=secondList.size();
-        
-        while(i<n && j<m){
-            int flf=firstList[i][0];
-            int fls=firstList[i][1];
-            int slf=secondList[j][0];
-            int sls=secondList[j][1];
-            
-            int a=max(flf,slf);
-            int b=min(fls,sls);
-            if(b>=a) ans.push_back({a,b});
-
-            if(fls>sls){
-                j++;
-            }
-            else if(sls>fls){
-                i++;
-            }
-            else{
-                i++;
-                j++;
-            }
+            if(store[i][1]>store[prev][1]) prev=i;
+            // continue;
         }
-        return ans;
+        else{
+            vector<int> temp;
+            // temp.push_back();
+            ans.push_back({max(store[prev][0],store[i][0]),min(store[prev][1],store[i][1])});
+        }
+        if(store[i][1]>store[prev][1]) prev=i;
+    }
+    return ans;
+
     }
 };
