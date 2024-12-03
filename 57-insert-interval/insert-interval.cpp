8c3513b55443a9db0b9541e4ed60d6a346259223
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-03 17:04:56 +0530

    Time: 7 ms (10.25%) | Memory: 24.6 MB (5.28%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,30 +2,41 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
-
-        // first jo jo overlap nhi kar rha usko as it it daal do
-        int i=0;
         int n=intervals.size();
-        while(i<n && newInterval[0]>intervals[i][1]){
-            ans.push_back({intervals[i][0],intervals[i][1]});
-            i++;
+        int flag=1;
+        // put new interval in new position
+        for(int i=0;i<n;i++){
+            if(flag&&newInterval[0]<intervals[i][0]){
+                ans.push_back(newInterval);
+                ans.push_back(intervals[i]);
+                flag=0;
+            }
+            else ans.push_back(intervals[i]);
         }
-        // now jo overlapping hai usko merge krdo
-        while(i<n && newInterval[1]>=intervals[i][0]){
-            newInterval[0]=min(newInterval[0],intervals[i][0]);
-            newInterval[1]=max(newInterval[1],intervals[i][1]);
-            i++;
+        if(flag) ans.push_back(newInterval);
+        // now simply apply merge intervals;
+        vector<vector<int>> finalAns;
+        for(int i=0;i<ans.size();i++){
+            if(finalAns.size()==0){
+                finalAns.push_back(ans[i]);
+            }
+            else{
+                auto prev=finalAns.back();
+                // finalAns.pop_back();
+                if(prev[1]<ans[i][0]){
+                    finalAns.push_back(ans[i]);
+                    continue;
+                }
+                else{
+                    finalAns.pop_back();
+                    vector<int> temp(2);
+                    temp[0]=min(prev[0],ans[i][0]);
+                    temp[1]=max(prev[1],ans[i][1]);
+                    finalAns.push_back(temp);
+                }
+            }
         }
-        // when we merged the over lapping thiing we will just add it 
-        ans.push_back(newInterval);
-        // after that just simply add remaining
-        while(i<n){
-            ans.push_back({intervals[i][0],intervals[i][1]});
-            i++;
-        }
-        
-
-        return ans;
-
+            return finalAns;
+            // return ans;
     }
 };
