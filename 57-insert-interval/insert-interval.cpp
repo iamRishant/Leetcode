aaab538f2ee2718dad608afbd321401c6a7d2321
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-03 17:29:08 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (7.13%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        // using 2 arrays but o(n) complexity
+        // divide the whole process into 3 parts
+        // first push all the interval which are smaller 
+        // then update the merged interval 
+        //last push all the remaining
+
         vector<vector<int>> ans;
         int n=intervals.size();
-        int flag=1;
-        // put new interval in new position
-        for(int i=0;i<n;i++){
-            if(flag&&newInterval[0]<intervals[i][0]){
-                ans.push_back(newInterval);
-                ans.push_back(intervals[i]);
-                flag=0;
-            }
-            else ans.push_back(intervals[i]);
+        int i=0;
+
+        // step1
+        while(i<n && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
         }
-        if(flag) ans.push_back(newInterval);
-        // now simply apply merge intervals;
-        vector<vector<int>> finalAns;
-        for(int i=0;i<ans.size();i++){
-            if(finalAns.size()==0){
-                finalAns.push_back(ans[i]);
-            }
-            else{
-                auto prev=finalAns.back();
-                // finalAns.pop_back();
-                if(prev[1]<ans[i][0]){
-                    finalAns.push_back(ans[i]);
-                    continue;
-                }
-                else{
-                    finalAns.pop_back();
-                    vector<int> temp(2);
-                    temp[0]=min(prev[0],ans[i][0]);
-                    temp[1]=max(prev[1],ans[i][1]);
-                    finalAns.push_back(temp);
-                }
-            }
+
+        // step2
+        // here we will update the interval , we will check number of interval which
+        // are intersecting with the interval and just merge them
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            i++;
         }
-            return finalAns;
-            // return ans;
+        ans.push_back(newInterval);
+        // step3 yha se aage koi over lap nhi kar rha hoga
+        while(i<n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
     }
 };
