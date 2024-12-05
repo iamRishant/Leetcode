e940575a5b0f05b2dcb6f82c24ad3559893cb425
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-05 13:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (9.15%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,20 +6,18 @@ public:
         int high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
-            // first check which side is sorted
+
             if(nums[mid]==target) return mid;
-            else if(nums[low]<=nums[mid]){
-                // it means left side is sorted
-                if(target<nums[mid] && target>=nums[low]) high=mid-1;
+            else if(nums[mid]>=nums[low]){
+                //it means left sorted
+                if(target>=nums[low] && target<nums[mid]) high=mid-1;
                 else low=mid+1;
             }
-            else if(nums[high]>=nums[mid]){
-                // it means right side is sorted
-                if(target > nums[mid] && target<=nums[high]) low=mid+1;
+            else{
+                if(target<=nums[high] && nums[mid]<target) low=mid+1;
                 else high=mid-1;
             }
         }
         return -1;
-        
     }
 };
