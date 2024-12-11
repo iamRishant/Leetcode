10b5e78d2319ed53aa4cfa07e5f1a4e89dcdaed6
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-11 21:14:26 +0530

    Time: 565 ms (19.34%) | Memory: 191.8 MB (21.45%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int maxi=1;
-        for(int i=0;i<nums.size();i++){
-            int idx=upper_bound(nums.begin(),nums.end(),nums[i]+(2*k))-nums.begin();
-            maxi=max(maxi,idx-i);
-        }
-        return maxi;
-
+            // lien sweep method
+            map<int,int> mp;
+            int ans = 0;
+            for(int i = 0;i<nums.size();i++){
+                    mp[nums[i] - k] ++;
+                    mp[nums[i] + k + 1] --;
+                    
+            }
+            int count=0;
+            // int ans=1;
+            for(auto [a,b] : mp){
+                    count+=b;
+                    ans = max(ans,count);
+            }
+            return ans;
+        
     }
 };
