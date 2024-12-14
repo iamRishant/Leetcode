2adacc26af1c27f46fd208dae3cef68cec1b1ee2
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-14 13:12:54 +0530

    Time: 237 ms (20.36%) | Memory: 156.1 MB (7.00%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        long long ans=0;
+        set<pair<int,int>> st;
+        // for(int i=0;i<nums.size();i++) st.insert({nums[i],i});
+        int n=nums.size();
+        int left=0;
+        for(int i=0;i<n;i++){
+            st.insert({nums[i],i});
+            int maxi=(st.rbegin()->first);
+            int mini=(st.begin()->first);
+            while(maxi-mini>2){
+                st.erase({nums[left],left});
+                left++;
+                maxi=(st.rbegin()->first);
+                mini=(st.begin()->first);
+            }
+            ans+=(i-left+1);
+        }
+        return ans;
+    }
+};
