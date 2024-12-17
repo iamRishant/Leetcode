c431819d882f2fbdde990e9f13d46fc157c06db8
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2024-12-17 16:47:35 +0530

    Time: 205 ms (5.06%) | Memory: 83.9 MB (5.39%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-        map<char,int> mp;
+        map<char,int> st;
         int n=s.length();
 
         for(int i=0;i<n;i++){
-            mp[s[i]]++;
+            st[s[i]]++;
         }
-        set<pair<char,int>> st;
-        for(auto it: mp){
-            st.insert({it.first,it.second});
-        } 
+        // set<pair<char,int>> st;
+        // for(auto it: mp){
+        //     st.insert({it.first,it.second});
+        // } 
         
         string ans="";
 
         while(!st.empty()){
             auto tp=*st.rbegin();
-            st.erase(tp);
+            st.erase(tp.first);
 
             if(tp.second<=repeatLimit){
                 int limit=tp.second;
@@ -33,15 +33,17 @@ public:
 
                 if(!st.empty()){
                     auto stp=*st.rbegin();
-                    st.erase(stp);
+                    st.erase(stp.first);
 
                     second+=stp.first;
 
                     if(stp.second-1>0){
-                        st.insert({stp.first,stp.second-1});
+                        st[stp.first]=stp.second-1;
+                        // st.insert({stp.first,stp.second-1});
                     }
                 }
-                st.insert({tp.first,tp.second-repeatLimit});
+                // st.insert({tp.first,tp.second-repeatLimit});
+                st[tp.first]=tp.second-repeatLimit;
                 if(second=="") return ans;
                 else ans+=second;
 
