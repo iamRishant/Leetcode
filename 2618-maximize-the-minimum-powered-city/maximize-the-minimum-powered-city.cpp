92288d750e5aafede18a2f36acd4131290b83719
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-01-15 11:55:49 +0530

    Time: 58 ms (97.34%) | Memory: 145.9 MB (78.76%) - LeetSync

--- 2618-maximize-the-minimum-powered-city/maximize-the-minimum-powered-city.cpp
+++ 2618-maximize-the-minimum-powered-city/maximize-the-minimum-powered-city.cpp
@@ -1,55 +1,43 @@
 class Solution {
 public:
-    bool check(long long mid, int k, int r,vector<int> stations){
-        int n=stations.size();
+    bool check(long long mid,vector<int> arr,int r,int k){
         long long sum=0;
-        for(int i=0;i<r;i++) sum+=stations[i];
-
+        for(int i=0;i<r;i++) sum+=arr[i];
+        int n=arr.size();
         for(int i=0;i<n;i++){
-            // moving window ahead
-            if(i+r<n) sum+=stations[i+r];
-            //piche wale ko removing
-            if(i-r-1>=0) sum-=stations[i-r-1];
+            // adjusting window
+            if(i+r<n){
+                sum+=arr[i+r];
+            }
+            if(i-r-1>=0) sum-=arr[i-r-1];
 
-            // now checking current power of station (sum is current power)
             if(sum<mid){
-                if(mid-sum>k) return false;//we dont have additional stations
-                else{
-                    //it means we have more req station 
-                    //we will plant that station fartherst so that it can power more citits
-                    stations[min(n-1,i+r)]+=(mid-sum);
-                    k-=(mid-sum);
-
-                    // now the power of the window is mid
-                    sum=mid;
-                }
+                //jitna req hai usse kam hai power
+                long long req=mid-sum;
+                if(req>k) return false;
+                arr[min(i+r,n-1)]+=req;
+                k-=req;
+                sum=mid;
             }
         }
-        // if all satisfied 
         return true;
     }
     long long maxPower(vector<int>& stations, int r, int k) {
-        long long high=k;
         long long low=0;
-        long long ans=-1;
+        long long high=k;
         int n=stations.size();
-        for(int i=0;i<n;i++){
-            high+=stations[i];
-        }
+        for(int i=0;i<n;i++) high+=stations[i];
 
-        
-       
+        long long ans=high;
         while(low<=high){
             long long mid=low+(high-low)/2;
-            if(check(mid,k,r,stations)){
+            if(check(mid,stations,r,k)){
                 ans=mid;
                 low=mid+1;
             }
             else high=mid-1;
         }
+
         return ans;
-
-
-        // return 0;
     }
 };
