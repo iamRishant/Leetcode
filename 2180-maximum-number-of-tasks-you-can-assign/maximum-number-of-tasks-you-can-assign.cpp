9712be2c0c74b2a73721669b65c7c44d56a546e7
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-01-17 15:07:56 +0530

    Time: 609 ms (79.63%) | Memory: 286.4 MB (55.56%) - LeetSync

--- 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.cpp
+++ 2180-maximum-number-of-tasks-you-can-assign/maximum-number-of-tasks-you-can-assign.cpp
@@ -1,52 +1,38 @@
 class Solution {
 public:
-
-    bool check(vector<int> &tasks,vector<int> &workers,int mid,int pills,int strength){
-        // we need to store mid number of strongest workers 
-        multiset<int> ms(workers.end()-mid,workers.end());
-        //now we will check if mid number of smaller tasks can be completed by these workers
+    bool check(int mid,vector<int> &tasks,vector<int> & workers,int pills,int strength){
+        multiset<int> st(workers.end()-mid,workers.end());
         for(int i=mid-1;i>=0;i--){
-            auto it=ms.end();
-            it--;//pointing on last worker
+            auto it=st.end();
+            it--;
 
-            if(tasks[i]> *it){
-                //means strength required to do the task is more than worker
-                // in this case first check do we have pills
-                if(!pills) return false;//if no pills then kucch nhi ho skta
-
-                // now check if we have any worker with strength matching with task
-                //here is why we need multiset coz we need sbse chota worker + strength who
-                // can complete the work so that worker with higher strength can do 
-                //other tasks
-                //worker+strength>=tasks[i] therefore worker>= tasks[i]-strength
-                //it means we need a worker whose strength is tasks[i]-strength atleast
-                // we will not delete the current worker we need to find new
-                it=ms.lower_bound(tasks[i]-strength);
-                if(it!=ms.end()) pills--;
+            if(tasks[i]>*it){
+                if(!pills) return false;
+                it=st.lower_bound(tasks[i]-strength);
+                if(it!=st.end()) pills--;
                 else return false;
             }
-            ms.erase(it); //worker to htanan pdega na
+            st.erase(it);
         }
-        //it means tasks hogya
         return true;
     }
     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
-        int n=tasks.size();
-        int m=workers.size();
         int low=0;
-        int high=min(n,m);
+        int high=min(tasks.size(),workers.size());
+
         sort(tasks.begin(),tasks.end());
         sort(workers.begin(),workers.end());
 
-        int ans=0;
+        int ans=-1;
+
         while(low<=high){
             int mid=low+(high-low)/2;
-
-            if(check(tasks,workers,mid,pills,strength)){
+            if(check(mid,tasks,workers,pills,strength)){
                 ans=mid;
                 low=mid+1;
             }
             else high=mid-1;
+                    
         }
         return ans;
     }
