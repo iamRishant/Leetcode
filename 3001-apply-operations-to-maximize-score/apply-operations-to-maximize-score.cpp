fa504f123ab5f6a94ae1a5eb7e8c348a6242717d
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-01-17 16:50:23 +0530

    Time: 707 ms (32.73%) | Memory: 297.7 MB (29.09%) - LeetSync

--- 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
+++ 3001-apply-operations-to-maximize-score/apply-operations-to-maximize-score.cpp
@@ -1,98 +1,92 @@
 class Solution {
 public:
-    vector<int> getNGL(vector<int> &ps){
+    vector<int> getngr(vector<int> &ps){
         stack<int> st;
         int n=ps.size();
-        vector<int> res(n);
-        for(int i=0;i<n;i++){
-            if(st.empty()){
-                res[i]=-1;
-                st.push(i);
-            }
-            else{
-                while(!st.empty() && ps[st.top()]<ps[i]) st.pop();
+        vector<int> ans(n);
 
-                if(st.empty()) res[i]=-1;
-                else res[i]=st.top();
-
-                st.push(i);
-            }
-        }
-        return res;
-    }
-
-    vector<int> getNGR(vector<int> &ps){
-        stack<int> st;
-        int n=ps.size();
-        vector<int> res(n);
         for(int i=n-1;i>=0;i--){
             if(st.empty()){
-                res[i]=n;
+                ans[i]=n;
                 st.push(i);
             }
             else{
                 while(!st.empty() && ps[st.top()]<=ps[i]) st.pop();
-
-                if(st.empty()) res[i]=n;
-                else res[i]=st.top();
+                if(st.empty()) ans[i]=n;
+                else ans[i]=st.top();
 
                 st.push(i);
+
             }
         }
-        return res;
+        return ans;
     }
-    int mod=1e9+7;
-    int modPow(int x, int y)
-    {
-        if (y == 0)
-            return 1;
-        long p = modPow(x, y / 2) % mod;
-        p = (p * p) % mod;
-        return y % 2 ? (p * x) % mod : p;
+    vector<int> getngl(vector<int> &ps){
+        stack<int> st;
+        int n=ps.size();
+        vector<int> ans(n);
+
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                ans[i]=-1;
+                st.push(i);
+            }
+            else{
+                while(!st.empty() && ps[st.top()]<ps[i]) st.pop();
+                if(st.empty()) ans[i]=-1;
+                else ans[i]=st.top();
+
+                st.push(i);
+
+            }
+        }
+        return ans;
+    }
+    int modPow(int num,int power){
+        int mod=1e9+7;
+        
+        if(power==0) return 1;
+        long long p=modPow(num,power/2)%mod;
+        p=(p*p)%mod;
+
+        return power%2 ? (p*num)%mod: p;
     }
     int maximumScore(vector<int>& nums, int k) {
         vector<int> all(1e5+1,0);
         for(int i=2;i<=1e5;i++){
             if(!all[i]){
-                for(int j=i;j<=1e5;j+=i){
+                for(int j=i;j<1e5;j+=i){
                     all[j]++;
                 }
             }
         }
         int n=nums.size();
         vector<int> ps(n,0);
-        for(int i=0;i<n;i++){
-            ps[i]=all[nums[i]];
-        }
-        vector<int> ngl,ngr;
-        ngl=getNGL(ps);
-        ngr=getNGR(ps);
-
+        for(int i=0;i<n;i++) ps[i]=all[nums[i]];
         vector<pair<int,int>> vec;
-        for(int i=0;i<n;i++){
-            vec.push_back({nums[i],i});
-        }
-
+        for(int i=0;i<n;i++) vec.push_back({nums[i],i});
         sort(vec.rbegin(),vec.rend());
-        long long res=1;
-        // long long mod=1e9+7;
+        vector<int> ngl,ngr;
+        ngl=getngl(ps);
+        ngr=getngr(ps);
+        long long ans=1;
+        int mod=1e9+7;
+
         for(int i=0;i<n;i++){
             int num=vec[i].first;
             int idx=vec[i].second;
-            // now we will check contribution of this number 
-            long long range=1L*(idx-ngl[idx])*(ngr[idx]-idx);
-            int operation;
-            if(k>range) operation=range;
-            else operation=k;
-            
 
-            res=res%mod*modPow(num,operation)%mod;
-            
+            long long contribution=1L*(idx-ngl[idx])*(ngr[idx]-idx);
+            int operation;
+            if(contribution > k) operation=k;
+            else operation=contribution;
+
+            ans=ans%mod*modPow(num,operation)%mod;
+            // ans=ans%mod;
 
             k-=operation;
-            if(k==0) return (int)res;
+            if(k==0) return (int)ans;
         }
-        return (int)res;
-
+        return ans;
     }
 };
