921bb02f423bbb2db2ec9309a2ca12cd601e1b59
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-01-27 16:42:23 +0530

    Time: 2 ms (31.76%) | Memory: 14.6 MB (38.16%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
-    void solve(int row,int col,vector<vector<int>> &vis){
+    void dfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>> &mat){
         vis[row][col]=1;
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
+        int delrow[]={1,0,-1,0};
+        int delcol[]={0,-1,0,1};
+
         for(int i=0;i<4;i++){
             int nrow=row+delrow[i];
             int ncol=col+delcol[i];
 
-            if(nrow>=0 && nrow<vis.size() && ncol>=0 && ncol<vis[0].size() && !vis[nrow][ncol]){
-                solve(nrow,ncol,vis);
+            if(nrow>=0 && ncol>=0 && nrow<mat.size() && ncol<mat[0].size() && !vis[nrow][ncol] && mat[nrow][ncol]=='O'){
+                dfs(nrow,ncol,vis,mat);
             }
         }
     }
-    void solve(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
+    void solve(vector<vector<char>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> vis(n,vector<int> (m,0));
+
+        // traverse boundary and run a dfs on all the boundary O's
+
+        for(int j=0;j<m;j++){
+            if(mat[0][j]=='O' && !vis[0][j]) dfs(0,j,vis,mat);
+            if(mat[n-1][j]=='O' && !vis[n-1][j]) dfs(n-1,j,vis,mat);
+        }
+        for(int i=0;i<n;i++){
+            if(mat[i][0]=='O' && !vis[i][0]) dfs(i,0,vis,mat);
+            if(mat[i][m-1]=='O' && !vis[i][m-1]) dfs(i,m-1,vis,mat);
+        }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]=='X') vis[i][j]=1;
+                if(mat[i][j]=='O' && !vis[i][j]) mat[i][j]='X';
             }
         }
         
-        // visiting first and last row
-
-        for(int col=0;col<m;col++){
-            if(!vis[0][col]) solve(0,col,vis);
-            if(!vis[n-1][col]) solve(n-1,col,vis);
-        }
-
-        for(int row=0;row<n;row++){
-            if(!vis[row][0]) solve(row,0,vis);
-            if(!vis[row][m-1]) solve(row,m-1,vis);
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]=='O' && !vis[i][j]) board[i][j]='X';
-            }
-        }
     }
 };
