f7fb9fb52c1336b0f1c43a19ccceba68bdd6b453
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-01-27 14:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (28.98%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int initialFresh=0;
-        int newRotten=0;
 
-        vector<vector<int>> vis(n,vector<int> (m,0));
-        queue<pair<int,pair<int,int>>> q;//time,row,col
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),0));
+
+        int fresh=0;
+
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==2){
+                    q.push({{i,j},0});
                     vis[i][j]=1;
-                    q.push({0,{i,j}});
                 }
-                else if(grid[i][j]==0) vis[i][j]=1;
-                else if(grid[i][j]==1) initialFresh++;
+                else if(grid[i][j]==1) fresh++;
+                else vis[i][j]=1;
             }
         }
 
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
-        int ans=0;
+        int countRotten=0;
+        int maxTime=0;
+        int delrow[]={1,0,-1,0};
+        int delcol[]={0,-1,0,1};
         while(!q.empty()){
             auto it=q.front();
-            int time=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
             q.pop();
-            ans=max(ans,time);
+            int row=it.first.first;
+            int col=it.first.second;
+            int time=it.second;
+            maxTime=max(time,maxTime);
             for(int i=0;i<4;i++){
                 int nrow=row+delrow[i];
                 int ncol=col+delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol]){
+                if(nrow>=0 && ncol>=0 && nrow<grid.size() && ncol<grid[0].size()  && !vis[nrow][ncol] ){
                     vis[nrow][ncol]=1;
-                    q.push({time+1,{nrow,ncol}});
-                    newRotten++;
+                    countRotten++;
+                    q.push({{nrow,ncol},time+1});
                 }
             }
-        }
-        if(initialFresh!=newRotten) return -1;
-        return ans;
+
+        } 
+        if(fresh!=countRotten) return -1;
+        return maxTime;
     }
 };
