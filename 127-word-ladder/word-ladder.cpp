6380c66a037b905a2f55db54270fb7b7862a0411
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-01-27 17:25:23 +0530

    Time: 348 ms (31.46%) | Memory: 21.3 MB (39.89%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -5,24 +5,28 @@ public:
         queue<pair<string,int>> q;
         q.push({beginWord,1});
         st.erase(beginWord);
+
         while(!q.empty()){
-            string word=q.front().first;
-            int step=q.front().second;
+            auto it=q.front();
             q.pop();
+            string word=it.first;
+            int step=it.second;
             if(word==endWord) return step;
 
-            for(int i=0;i<word.length();i++){
+            for(int i=0;i<word.size();i++){
+                // we will linearly change each character and check if it exists or not
                 char original=word[i];
                 for(char c='a';c<='z';c++){
                     word[i]=c;
                     if(st.find(word)!=st.end()){
-                        st.erase(word);
                         q.push({word,step+1});
+                        st.erase(word);
                     }
                 }
                 word[i]=original;
             }
         }
-            return 0;
+        return 0;
+
     }
 };
