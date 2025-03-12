ff9d68604b9d3607b42516d732b7a9e36d60eb62
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-03-12 11:14:12 +0530

    Time: 66 ms (41.74%) | Memory: 47.7 MB (31.75%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -7,11 +7,11 @@ public:
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             if(sum==k) ans++;
+
             int rem=sum-k;
             if(mp.find(rem)!=mp.end()){
                 ans+=mp[rem];
             }
-
             mp[sum]++;
         }
         return ans;
