ea5a285b4c86adc626f42baf41cff996128290a1
Rishant Rana <98682165+iamRishant@users.noreply.github.com>
2025-04-13 11:05:04 +0530

    Time: 5 ms (34.73%) | Memory: 10.8 MB (97.07%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<char> st;
-        int n=num.size();
+        string res="";
+        int n=num.length();
+
         for(int i=0;i<n;i++){
-            while(!st.empty() && k>0 && (st.top()-'0')>(num[i]-'0')){
-                st.pop();
+            while(res.length()>0 && res.back()>num[i] && k>0){
                 k--;
+                res.pop_back();
             }
-            st.push(num[i]);
+            if(res.length()>0 || num[i]!='0'){
+                res.push_back(num[i]);
+            }
         }
-        while(k>0){
-            st.pop();
-            k--;
-            //deleting from back coz in the back there will be larget number
-        }
-        // ab iske baad stack pura ka pura empty ho skta hai so lets check
-        if(st.empty()) return "0";
-        // ab empty nhi hai then res me value store krlo
-        string ans="";
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        // ab problem ye hai ki ans ke piche 0 ho skta hai jo ki count nhi krna hai to usko deletr krte hai
-        while(ans.length()!=0 && ans.back()=='0'){
-            ans.pop_back();
-        } 
-        // ab iske baad bhi ho skta hai sbb zero ho so lets check
-        if(ans.length()==0) return "0";
-        
-        //ab kyuki stack se nikale the to ulta tha to sidha kiye
-        reverse(ans.begin(),ans.end());
-        return ans;
+        while(k>0 && res.length()>0) {res.pop_back();k--;}
+        if(res.length()==0) return "0";
+
+        return res;
     }
 };
